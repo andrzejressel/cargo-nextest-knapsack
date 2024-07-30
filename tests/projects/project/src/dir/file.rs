3c84ea9511321238c3ce84b64bd70327a0e64569
Andrzej Ressel <github@andrzejressel.pl>
2024-07-31 01:37:19 +0200

    Fix test name matching

--- tests/projects/project/src/dir/file.rs
+++ tests/projects/project/src/dir/file.rs
@@ -4,4 +4,10 @@ mod tests {
     fn test_in_subdirectory() {
         assert_eq!(1, 1)
     }
+
+    // Tests matching of test names
+    #[test]
+    fn test_in_subdirectory_2() {
+        assert_eq!(1, 1)
+    }
 }
